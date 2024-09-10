ed36c2c398fb6e447196a4265190c197bf98014c
linikers <linikers@hotmail.com>
2024-09-10 15:07:38 -0300

    limpando a base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Rocket Studio",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
 import { Facebook, Instagram, Pinterest } from "@mui/icons-material";
-import { Box, Grid2, IconButton, Typography } from "@mui/material";
+import { Box, Button, Grid2, IconButton, Typography } from "@mui/material";
+import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <Box>
+      <Head>
+        <title>Rocket Studio Tattoo</title>
+      </Head>
       <Grid2>
         <Typography>Rocket Studio</Typography>
       </Grid2>
+      <Box>
+        <Button>Home</Button>
+        <Button>Portfolio</Button>
+        <Button>Contato</Button>
+        <Link href="/">Home</Link>
+        <Link href="/portfolio">Portfolio</Link>
+        <Link href="/contato">Contato</Link>
+      </Box>
       <Grid2>
         <Box>
         <IconButton href="https://pinterest.com" target="_blank" aria-label="Pinterest">
